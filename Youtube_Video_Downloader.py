676c039b813094e92a7e74c6125bd77df5c9b25b
Dark2343 <94694375+Dark2343@users.noreply.github.com>
2023-04-26 08:32:00 +0200

    Optimized the code

--- Youtube_Video_Downloader.py
+++ Youtube_Video_Downloader.py
@@ -1,79 +1,38 @@
-# Date: 8/3/2022
-# A program to download videos from Youtube as either a video format or an audio one
-# Allows you to choose the quality if you're downloading it as a video while also telling you the size of the download
-
-
 import time
 from pytube import YouTube
 from pytube.cli import on_progress
+import ssl
 
 url = str(input("Enter the URL of the video you wish to download:\n"))
-video = YouTube(url, on_progress_callback=on_progress)
-print(f'\nTitle: {video.title}')
-print(f'Length: {round(video.length/60)} mins')
-print(f'Made by: {video.author}')
+link = YouTube(url, on_progress_callback=on_progress)
+print(f'\nTitle: {link.title}\nLength: {round(link.length/60)} mins\nMade by: {link.author}')
 type = int(input("\nDo you want to download Video or Audio:\n 1- Video\n 2- Audio\n"))
+media = link.streams.filter(progressive=True, res="144p").first()
 
 if type == 1:
     quality = int(input("\nChoose your preferred quality:\n 1- 144p\n 2- 360p\n 3- 720p\n"))
+    
     if quality == 1:
-        video = video.streams.filter(progressive=True, res="144p").first()
-        if video:
-            print(f'\nFile size is: {round(video.filesize / 10 ** 6)} MBs')
-            download = int(input("Do you wish to download this file ?\n 1-Yes\n 2-No\n"))
-
-            if download == 1:
-                print("Please wait, file is being downloaded...\n")
-                video.download('/Downloads')
-                print("File has been downloaded\n Enjoy :D\n")
-            elif download == 2:
-                print("Program has been terminated")
-        elif not video:
-            print("Sorry this quality is unavailable")
-
+        media = link.streams.filter(progressive=True, res="144p").first()
     elif quality == 2:
-        video = video.streams.filter(progressive=True, res="360p").first()
-        if video:
-            print(f'\nFile size is: {round(video.filesize / 10 ** 6)} MBs')
-            download = int(input("Do you wish to download this file ?\n 1-Yes\n 2-No\n"))
-
-            if download == 1:
-                print("Please wait, file is being downloaded...\n")
-                video.download('/Downloads')
-                print("File has been downloaded\n Enjoy :D\n")
-            elif download == 2:
-                print("Program has been terminated")
-        elif not video:
-            print("Sorry this quality is unavailable")
-
+        media = link.streams.filter(progressive=True, res="360p").first()
     elif quality == 3:
-        video = video.streams.filter(progressive=True, res="720p").first()
-        if video:
-            print(f'\nFile size is: {round(video.filesize / 10 ** 6)} MBs')
-            download = int(input("Do you wish to download this file ?\n 1-Yes\n 2-No\n"))
-
-            if download == 1:
-                print("Please wait, file is being downloaded...\n")
-                video.download('/Downloads')
-                print("File has been downloaded\n Enjoy :D\n")
-            elif download == 2:
-                print("Program has been terminated")
-        elif not video:
-            print("Sorry this quality is unavailable")
+        media = link.streams.filter(progressive=True, res="720p").first()
 
 elif type == 2:
-    video = video.streams.filter(mime_type="audio/mp4", abr="128kbps", type="audio").first()
-    if video:
-        print(f'\nFile size is: {round(video.filesize / 10 ** 6)} MBs')
-        download = int(input("Do you wish to download this file ?\n 1-Yes\n 2-No\n"))
+    media = link.streams.filter(mime_type="audio/mp4", abr="128kbps", type="audio").first()
 
-        if download == 1:
-            print("Please wait, file is being downloaded...\n")
-            video.download('/Downloads')
-            print("File has been downloaded\nEnjoy :D\n")
-        elif download == 2:
-            print("Program has been terminated")
-    elif not video:
-        print("Sorry this format is unavailable")
+if media:
+    print(f'\nFile size is: {round(media.filesize / 10 ** 6)} MBs')
+    download = int(input("Do you wish to download this file ?\n 1-Yes\n 2-No\n"))
+
+    if download == 1:
+        print("Please wait, file is being downloaded...\n")
+        media.download('/Downloads')
+        print("File has been downloaded\nEnjoy :D\n")
+    elif download == 2:
+        print("Program has been terminated")
+elif not media:
+    print("Sorry this format is unavailable")
 
 time.sleep(5)
