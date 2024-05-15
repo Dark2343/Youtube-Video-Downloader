06d24ed76dd502372e9165b0ea95abba1ec8e1f4
Dark <mohamed2003elashmawy@gmail.com>
2024-05-15 04:29:32 +0300

    Implemented a download bar that dynamically changes as the download progresses

--- downloadPage.py
+++ downloadPage.py
@@ -2,7 +2,7 @@
 # Message at the end
 # Return to main menu
 
-import logic
+import logic, threading
 import customtkinter as ctk
 
 def show(app, path, media, url):
@@ -17,5 +17,25 @@ def show(app, path, media, url):
     progressBar = ctk.CTkProgressBar(app, width=804, height=20,  orientation= "horizontal", mode= "determinate")
     progressBar.place(relx = 0.03, rely = 0.35)
     
-    logic.download(media, path, progressBar)
+    # Updates the progress every 100 ms until the download is finished
+    def updateProgressBar():
+        progressBar.set(logic.progress/ 100)
+        if logic.progress < 100:
+            app.after(100, updateProgressBar)
+        else:
+            # Show message at the end of download
+            completeLabel = ctk.CTkLabel(app, text="Download Complete", font=("", 15))
+            completeLabel.place(relx=0.02, rely=0.45)
     
+    # Starts downloading the video on a separate thread to ensure that the download and updating of progress bar don't interrupt each other
+    def downloadVideo():
+        def downloadAsync():
+            logic.download(media, path, progressBar)
+    
+        # Starting the download on a new thread
+        downloadThread = threading.Thread(target= downloadAsync)
+        downloadThread.start()
+        # Starts updating the progress bar
+        updateProgressBar()
+    
+    downloadVideo()
--- logic.py
+++ logic.py
@@ -1,17 +1,3 @@
-
-# TODO
-# Need to add GUI
-
-# Date: 8/3/2022
-# A program to download videos from YouTube as either a video format or an audio one
-# Allows you to choose the quality if you're downloading it as a video while also telling you the size of the download
-
-# Date: 26/4/2023
-# Optimized it and removed repeated code... also broke it (Ver. 12)
-
-# Date: 13/5/2023
-# Code no longer broken (had to update the module Ver. 15), also added QoL improvements
-
 from pytube import YouTube
 
 progress = 0
@@ -31,45 +17,5 @@ def getData(url):
 def download(media, path, progressBar):
     global progress
     
-    progressBar.set(progress)
+    progress = 0
     media.download(path)
-
-# print(f'Title: {link.title}\nLength: {round(link.length/60)} mins\nMade by: {link.author}')
-
-# type = int(input("\nDo you want to download this as Audio or Video:\n 1- Audio\n 2- Video\n"))
-# media = link.streams.filter(progressive=True, res="144p").first()
-
-
-# if type == 1:
-#     media = link.streams.filter(mime_type="audio/mp4", abr="128kbps", type="audio").first()
-
-# elif type == 2:
-#     quality = int(input("\nChoose your preferred quality:\n 1- 144p\n 2- 360p\n 3- 720p\n"))
-    
-#     if quality == 1:
-#         media = link.streams.filter(progressive=True, res="144p").first()
-
-#     elif quality == 2:
-#         media = link.streams.filter(progressive=True, res="360p").first()
-
-#     elif quality == 3:
-#         media = link.streams.filter(progressive=True, res="720p").first()
-
-# if media:
-#     os.system('cls')
-#     print(f'File size is: {round(media.filesize / 10 ** 6)} MBs')
-#     download = int(input("Do you wish to download this file ?\n 1-Yes\n 2-No\n"))
-
-#     if download == 1:
-#         os.system('cls')
-#         print("Please wait, file is being downloaded...\n")
-#         media.download('D:\\Downloads')
-#         print("\n\nFile has been downloaded\nEnjoy :D\n")
-
-#     elif download == 2:
-#         print("Program has been terminated")
-
-# elif not media:
-#     print("Sorry this format is unavailable")
-
-# time.sleep(5)
